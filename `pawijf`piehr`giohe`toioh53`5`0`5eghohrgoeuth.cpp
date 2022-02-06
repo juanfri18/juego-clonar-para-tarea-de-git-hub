ab8b93d427bd787ca5067546820cc3760f66249e
juanfri18 <94106447+juanfri18@users.noreply.github.com>
2022-02-06 13:38:07 +0100

    Add files via upload

---
+++ `pawijf`piehr`giohe`toioh53`5`0`5eghohrgoeuth.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	string nombre;
+	cout<<"por favor escriba su nombre\n";
+	cin>>nombre;
+	cout<<"encantado " + nombre + "\n";
+	return 0;
+}
+
