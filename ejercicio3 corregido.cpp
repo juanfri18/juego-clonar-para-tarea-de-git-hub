11bdab6ddd433176bca61c61344a24fb692bcba2
juanfri18 <94106447+juanfri18@users.noreply.github.com>
2022-02-06 13:06:30 +0100

    Add files via upload

---
+++ ejercicio3 corregido.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int calculador (int num);
+
+int main(int argc, char *argv[]) {
+	
+	
+	cout <<"por favor escriba un numero \n";
+	int numero;
+	cin >> numero;
+	cout<< calculador(numero);
+	
+}
+int calculador (int numero)
+{	
+	
+
+if (numero>0) {
+
+	
+	cout<<"su numero es positivo\n";
+	return 0;
+}
+else ;
+{
+	cout<<"su numero es negativo\n";
+
+}
+return 0;
+}
+
+
